d658f857e44aa85a1706fe64c10349c163853b0d
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-06-30 16:53:38 +0200

    Create 100-createIteratorObject.js

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,21 @@
+export default function createIteratorObject(report) {
+  /* eslint-disable */
+  let departments = Object.values(report);
+  let employees = [];
+
+  for (let department of departments) {
+    employees.push(...department);
+  }
+
+  let index = 0;
+
+  return {
+    next: function() {
+      if (index < employees.length) {
+        return { value: employees[index++], done: false };
+      } else {
+        return { done: true };
+      }
+    }
+  };
+}
