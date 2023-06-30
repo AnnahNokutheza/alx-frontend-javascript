65ecc30d43a80c077e67a802f109125511e8a963
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-06-30 17:27:50 +0200

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,8 @@
 import ClassRoom from './0-classroom';
 
+const ClassRoomList = []; // Define the ClassRoomList array
+ /* eslint-disable */
 export default function initializeRooms() {
-  /* eslint-disable */
   const class1 = new ClassRoom(19);
   const class2 = new ClassRoom(20);
   const class3 = new ClassRoom(34);
