1b523f79bda97dc89eddcd1e5f8c6839e8b29df5
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-06-30 17:19:11 +0200

    Update 101-main.js

--- 0x00-ES6_basic/101-main.js
+++ 0x00-ES6_basic/101-main.js
@@ -1,16 +1,16 @@
-import createIteratorObject, { iterateThroughObject } from './101-iterateThroughObject.js';
+import createEmployeesObject from "./11-createEmployeesObject.js";
+import createReportObject from './12-createReportObject.js';
+import createIteratorObject from './100-createIteratorObject.js';
+import iterateThroughObject from './101-iterateThroughObject.js';
 
-const report = {
-  allEmployees: {
-    engineering: [
-      'John Doe',
-      'Guillaume Salva',
-    ],
-    // ...
-  },
+
+const employees = {
+    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+    ...createEmployeesObject('marketing', ['Sylvie'])
 };
 
-const iterator = createIteratorObject(report);
-const result = iterateThroughObject(iterator);
-console.log(result);
+const report = createReportObject(employees);
+const reportWithIterator = createIteratorObject(report);
+
+console.log(iterateThroughObject(reportWithIterator));
 
