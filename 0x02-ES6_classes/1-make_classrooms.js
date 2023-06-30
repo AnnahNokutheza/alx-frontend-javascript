f3d60610eac725a8deb9c625a154494db0e4b251
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-06-30 17:34:23 +0200

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,24 +1,12 @@
 import ClassRoom from './0-classroom';
 
-const ClassRoomList = [];
+const ClassRoomList = []; // Define the ClassRoomList array
 
 export default function initializeRooms() {
- /* eslint-disable */
   const class1 = new ClassRoom(19);
   const class2 = new ClassRoom(20);
   const class3 = new ClassRoom(34);
-
-  // Check if each ClassRoom instance has the correct max student size
-  if (class1.getMaxStudents() !== 19 || class2.getMaxStudents() !== 20 || class3.getMaxStudents() !== 34) {
-    throw new Error('Incorrect max student size');
-  }
-
   ClassRoomList.push(class1, class2, class3);
-
-  // Check if the returned array has exactly three ClassRoom instances
-  if (ClassRoomList.length !== 3) {
-    throw new Error('Incorrect number of ClassRoom instances');
-  }
-
   return ClassRoomList;
 }
+
