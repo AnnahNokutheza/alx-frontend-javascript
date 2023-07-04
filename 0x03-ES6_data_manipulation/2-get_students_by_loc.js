48bde7a0243dd4c82fa90866aeebfa806045fde1
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-07-04 17:06:45 +0200

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,4 +2,18 @@ function getStudentIdsSum(students) {
   return students.reduce((sum, student) => sum + student.id, 0);
 }
 
-export default getStudentIdsSum;
+// Assuming getListStudents is defined and returns an array of students
+function getListStudents() {
+  // Replace this with your implementation to fetch or generate the list of students
+  // For demonstration purposes, we'll return a sample array of students
+  return [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Alice' },
+    { id: 3, name: 'Bob' },
+    { id: 4, name: 'Eve' }
+  ];
+}
+
+const students = getListStudents();
+const sumOfIds = getStudentIdsSum(students);
+console.log(sumOfIds);
