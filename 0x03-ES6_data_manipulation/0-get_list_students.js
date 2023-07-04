17bef906fdabac08e5c3c4dbe23f186c224b4b43
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-07-04 17:23:06 +0200

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,5 @@
-export default function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
+function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
 }
+
+export default getStudentsByLocation;
