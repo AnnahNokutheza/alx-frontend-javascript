ebc557d960cb75dd6d1d7e5dc59ff0935913cc5a
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-07-04 17:31:34 +0200

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,8 @@
-const getStudentIdsSum = (students) => students.reduce((preVal, cur) => preVal + cur.id, 0);
+// The reduce method works with two functions called accumulator and current
+// The current value should be 0 by default
+// The accumulator adds upp all the specified valued
+export default function getStudentIdsSum(arr) {
+  return arr.reduce((acc, cur) => acc + cur.id, 0);
+}const getStudentIdsSum = (students) => students.reduce((preVal, cur) => preVal + cur.id, 0);
 
 export default getStudentIdsSum;
